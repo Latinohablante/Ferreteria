8b87c0b158377eec46666df75ed4dbd9e1a264ad
christiancelis <73555661+christiancelis@users.noreply.github.com>
2024-02-24 14:58:57 -0500

    aa

---
+++ db/db.js
@@ -0,0 +1,19 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAT2Qj9OkOke7-bc4uHsmsOm3DGBsNxU5o",
+    authDomain: "ferreteriakks.firebaseapp.com",
+    projectId: "ferreteriakks",
+    storageBucket: "ferreteriakks.appspot.com",
+    messagingSenderId: "814841204472",
+    appId: "1:814841204472:web:0a8751b8e07f66482eb50d",
+    measurementId: "G-B8G2EC8GVL"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+
+
+
+
