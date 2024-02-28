0941fa90a2c9d131ad3f89f4f1d73cec74703678
christiancelis <73555661+christiancelis@users.noreply.github.com>
2024-02-28 09:02:29 -0500

    conexion cambiada

--- views/mostrarProductos.js
+++ views/mostrarProductos.js
@@ -4,13 +4,7 @@ import {} from "./iniciosesion.js"
 
 control("ObtenerProducto");
 
-
 let categorias = [];
-
-let cont= 0
-console.log(categorias)
-
-
 
 let listaproductos = []
 
@@ -66,12 +60,7 @@ export function mostrarProductosEnHTML(categorias) {
   enviarbusqueda.addEventListener("click",()=>{
     let bproducto = document.getElementById("bproducto")
     let valorbusqueda = bproducto.value
-    console.log(typeof(valorbusqueda))
-    console.log(listaproductos)
-    let lp = listaproductos.filter( (list)=>{
-        list.nombre.includes(valorbusqueda)
-    });
-    console.log(lp)
+    console.log("")
   
   })
 
@@ -106,46 +95,6 @@ export function Mostrarproductos(data) {
   }
 }
 
-
-let ves = 0;
-
-// document.addEventListener("DOMContentLoaded", function () {
-
-//   let contador = document.getElementById("DatosContador")
-//   contador.textContent = 0;
-
-//   let cont = document.getElementById('aumentar');
-//   let contMenos = document.getElementById('disminuir');
-
-//   if (cont && contMenos && p) {
-//     cont.addEventListener('click', () => {
-//       ves++;
-//       contador.textContent = ves;
-//     });
-
-//     contMenos.addEventListener('click', () => {
-//       if (ves > 0) {
-//         ves--;
-//       }
-//       contador.textContent = ves;
-//     });
-//   } else {
-//     console.error("No se encontraron algunos elementos en el DOM.");
-//   }
-// });
-
-//window.addEventListener("DOMContentLoaded", function (event) {
-    
-//});
-
-
-
-  // window.addEventListener("load", function (event) {
-  //   let btnAumentar = document.getElementById("aumentar")
-  //   btnAumentar.addEventListener("click",()=>{
-  //     console.log("btn aumentar")
-  //   })
-  // });
 
 
 
