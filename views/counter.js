45e88dcc0e8daa850df75f0c081e40abea05fa11
Carlos Jhoan Aguilar Galvis <carlosjhoanaguilar@gmail.com>
2024-02-28 15:09:04 -0500

    Se empezó a extraer productos para ponerlos en el carrito

--- views/counter.js
+++ views/counter.js
@@ -1,36 +1,56 @@
 
 
+export let arrayProductosCarrito = [];
+
 export let counter = document.addEventListener("DOMContentLoaded", () => {
     let cont;
     let numCont;
+    let numDIvCont;
 
     document.body.addEventListener("click", (e) => {
         
         const carritoDiv = document.getElementsByClassName("NumCarrito");
         let numCarrito = carritoDiv[0].innerText;
-        //console.log(carritoDiv[0].innerText);
+        let arrayProductoClick = [];
+        let nombreProducto;
+        let imgProducto;
+        let precioProducto;
+
         if (e.target.innerText == "+") {
-            //console.log("DEBO SUMAR");
-            console.log("El div que hay arriba", document.elementFromPoint(e.x + 30, e.y).children[0].innerText)
-            //console.log("Precio", document.elementFromPoint(e.x, e.y - 20).children[2].innerText)
+            nombreProducto = document.elementFromPoint(e.x + 30, e.y).children[0].innerText;
+            imgProducto =  document.elementFromPoint(e.x, e.y - 20).children[2].attributes[0].value;
+            precioProducto = document.elementFromPoint(e.x, e.y - 20).children[3].innerText;
+
+            //console.log("El div que hay arriba", document.elementFromPoint(e.x + 30, e.y).children[0].innerText)
+            console.log("Precio", precioProducto.split(":")[1].trim());
             numCarrito = carritoDiv[0].innerText;
             cont = document.elementFromPoint(e.x - 30, e.y);
             numCont = document.elementFromPoint(e.x - 30, e.y).innerText;
             document.elementFromPoint(e.x -30, e.y).innerText = `${Number(numCont) + 1}`
+            numDIvCont = document.elementFromPoint(e.x - 30, e.y).innerText;
             if (numCarrito == "") {
                 carritoDiv[0].innerText = "1";
             } else {
                 carritoDiv[0].innerText = `${Number(numCarrito) + 1}`
             }
-            //console.log(e.x, e.y);
+
+            if (arrayProductosCarrito.length === 0) {
+
+            }
+            arrayProductoClick.push(nombreProducto, imgProducto, precioProducto.split(":")[1].trim(), numDIvCont);
+            console.log("array Producto", arrayProductoClick)
+
+            
         } else if (e.target.innerText == "-") {
             numCarrito = carritoDiv[0].innerText;
             cont = document.elementFromPoint(e.x + 30, e.y);
             numCont = document.elementFromPoint(e.x + 30, e.y).innerText;
             if (Number(numCont) <= 0) {
                 document.elementFromPoint(e.x + 30, e.y).innerText == "0"
+                numDIvCont = document.elementFromPoint(e.x + 30, e.y).innerText;
             } else {
                 document.elementFromPoint(e.x + 30, e.y).innerText = `${Number(numCont) - 1}`
+                numDIvCont = document.elementFromPoint(e.x + 30, e.y).innerText;
                 carritoDiv[0].innerText = `${Number(numCarrito) - 1}`;
             }
 
